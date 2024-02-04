a8eceb1556d184405f605a6fd1841b6c791acb40
Sachin Singh <86557710+sachin1singh@users.noreply.github.com>
2024-02-05 01:58:45 +0530

    Time: 1363 ms (39.22%) | Memory: 0B (100.00%) - LeetSync

--- 1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
+++ 1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
@@ -2,4 +2,4 @@
 SELECT user_id, name, mail
 FROM Users
 WHERE mail LIKE '%@leetcode.com%'
-  AND mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@[a-zA-Z0-9.-]*leetcode\\.com$'AND (mail LIKE '%_%' OR mail LIKE '%-%' or mail like '%-@leetcode.com%' )
+  AND mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@[a-zA-Z0-9.-]*leetcode\\.com$'
