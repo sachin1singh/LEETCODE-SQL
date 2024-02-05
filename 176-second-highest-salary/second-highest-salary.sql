eb76b227b72e1c3bebc521c14ade9414d4b15308
Sachin Singh <86557710+sachin1singh@users.noreply.github.com>
2024-02-05 13:27:21 +0530

    Time: 1216 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,8 +1,4 @@
 # Write your MySQL query statement below
-/*select (salary) as SecondHighestSalary from Employee
-if (salary isnull)then 1 else 0
-order by salary
-limit 1 offset 1 */
 
 select
 (select distinct Salary 
