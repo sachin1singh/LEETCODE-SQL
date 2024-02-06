372810fabc8ae491bcc9a9d373843a4ccbbee6ed
Sachin Singh <86557710+sachin1singh@users.noreply.github.com>
2024-02-06 22:10:43 +0530

    Time: 1328 ms (29.83%) | Memory: 0B (100.00%) - LeetSync

--- 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -1,14 +1,6 @@
-/*select a.employee_id,a.name,count(a.reports_to) as reports_count,round(avg(a.age)) as average_age
-from Employees a inner join Employees b on a.employee_id =b.employee_id 
-order by a.employee_id*/
+select a.reports_to as employee_id,b.name,count(b.employee_id) as reports_count,coalesce(cast(avg(a.age) as unsigned), 0) as average_age
+from Employees a left join Employees b on b.employee_id =a.reports_to
+where a.reports_to is not null
+group by a.reports_to,b.name
+order by a.reports_to
 
-SELECT
-    m.reports_to AS employee_id,
-    e.name,
-    COUNT(e.employee_id) AS reports_count,
-    COALESCE(CAST(AVG(m.age) AS UNSIGNED), 0) AS average_age
-FROM employees m
-LEFT JOIN employees e ON e.employee_id = m.reports_to
-WHERE m.reports_to IS NOT NULL
-GROUP BY m.reports_to, e.name
-ORDER BY m.reports_to;
